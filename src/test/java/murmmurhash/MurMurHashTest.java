d253184668bbecacf4e53780fa12f9820624aa45
Arxemond777 <1arxemond1@gmail.com>
2019-07-24 14:30:18 +0300

    the readmemd has been updated

--- src/test/java/murmmurhash/MurMurHashTest.java
+++ src/test/java/murmmurhash/MurMurHashTest.java
@@ -11,7 +11,7 @@ import static murmurhash.enums.KeysSplitConfig.*;
  */
 public class MurMurHashTest {
     public static final String USER_ID = "abcdefghijklmnopqrstuvwxyz";
-    public static final String USER_ID_1 = "5a2ce01f27a9ab602e2d1834";
+    public static final String USER_ID_1 = "5a2ce01f27a9a1b602e2d1834";
     @Test
     public void basicTestCorrectSpitKey1() {
         assertEquals(8, new MurMurHash().getSplit(USER_ID, KEY_8_M).intValueExact());
@@ -29,8 +29,8 @@ public class MurMurHashTest {
 
     @Test
     public void basicTestCorrectSpitKey4() {
-        assertEquals(7, new MurMurHash().getSplit(USER_ID_1, KEY_8_A).intValueExact());
+        assertEquals(2, new MurMurHash().getSplit(USER_ID_1, KEY_8_A).intValueExact());
         assertEquals(7, new MurMurHash().getSplit(USER_ID_1, KEY_8_C).intValueExact());
-        assertEquals(1, new MurMurHash().getSplit(USER_ID_1, KEY_8_M).intValueExact());
+        assertEquals(4, new MurMurHash().getSplit(USER_ID_1, KEY_8_M).intValueExact());
     }
 }
